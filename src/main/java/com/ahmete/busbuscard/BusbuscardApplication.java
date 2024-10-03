36423adb6e7aa42944cbec284bbf41f5f4fea027
AhmetEris-dev <ahmet2_lock@hotmail.com>
2024-10-03 15:29:13 +0300

    first commit

---
+++ src/main/java/com/ahmete/busbuscard/BusbuscardApplication.java
@@ -0,0 +1,13 @@
+package com.ahmete.busbuscard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusbuscardApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(BusbuscardApplication.class, args);
+	}
+	
+}
