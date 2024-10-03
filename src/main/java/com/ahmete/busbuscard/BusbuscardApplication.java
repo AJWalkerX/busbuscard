804d39a1f3ee738a19ec622e9b578aeea47533ba
Alexander Joseph Walker <a.j.walker99@outlook.com>
2024-10-03 15:38:03 +0300

    Test Commit

--- src/main/java/com/ahmete/busbuscard/BusbuscardApplication.java
+++ src/main/java/com/ahmete/busbuscard/BusbuscardApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BusbuscardApplication {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(BusbuscardApplication.class, args);
+//		SpringApplication.run(BusbuscardApplication.class, args);
+		System.out.println("Alex konsol çıktısı veriyor!");
 	}
 	
 }
